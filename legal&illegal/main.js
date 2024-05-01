dd3894ca3532ccd2c8075c5dc5246edfc06c884c
Arishah-Khan <arishahsanaullah78@gmail.com>
2024-05-01 07:25:46 +0500

    Chap 2,3,4

---
+++ legal&illegal/main.js
@@ -0,0 +1,49 @@
+// 1. Declare 3 variables in one statement.
+
+// var name;
+// age;
+// job;
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// Legal
+
+// var $name;
+// var _name;
+// var name1;
+// var myName;
+// var MyName;
+
+// Illegal
+
+// var 1name;
+// var name@;
+// var name#;
+// var my name;
+// var @name
+
+
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+// document.write(<h1>"Rules for naming JS variables"</h1>);
+
+// document.write("Variable names can only contain numbers, $ and _.For example: $my_1stVariable");
+
+// document.write("Variable names can only contain with a letter, $ or _.For Example: $name,_name or name");
+
+// document.write("Variable names are case sensitive");
+
+// document.write("Variable names should not be JS keywords");
+
+
+
