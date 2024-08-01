1b61300e3785bfd963411ae44ef1ef5828231260
Arishah-Khan <arishahsanaullah78@gmail.com>
2024-08-01 14:59:59 +0500

    Done

---
+++ Chap43-48(Events)/main.js
@@ -0,0 +1,63 @@
+// 1. Show an alert box on click on a link.
+
+// function alertBox(){
+//     alert("You clicked on a link!");
+// }
+
+// 2. Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
+
+// function iphone(){
+//     alert("Thanks for purchasing iPhone!");
+// };
+
+// function samsung(){
+//     alert("Thanks for purchasing Samsung!");
+// };
+
+// function oppo(){
+//     alert("Thanks for purchasing OPPO!");
+// };
+
+// function reno(){
+//     alert("Thanks for purchasing Redmi!");
+// };
+
+// 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted.
+
+// function deleteRow(e){
+//     e.parentNode.parentNode.remove()
+// }
+
+// 4. Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+
+// in index.html page code is written
+
+// 5. Show a counter in browser. Counter should increase on click on increase
+// button and decrease on click on decrease button. And show updated counter
+// value in browser.
+
+// function incrementCounter(){
+//     let counter = document.getElementById("count");
+
+//     counter.innerText = parseInt(counter.innerText);
+
+//     counter.innerText++;
+// }
+
+// function decrementCounter(){
+//     let counter = document.getElementById("count");
+    
+//     counter.innerText = parseInt(counter.innerText);
+    
+//     if(counter.innerText > 0){
+//         counter.innerText--;
+//     }
+//     else{
+//         alert("Counter cannot go below 0");
+//     }
+// }
+
